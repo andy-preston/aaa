457bdd0aeae1c8ada48e5c2ef4e99b2b68cf18f9
Andy Preston <edgeeffect@gmail.com>
2024-08-09 18:47:02 +0100

    Favour hash-maps over objects (missing instructions)

--- src/instructionSet.ts
+++ src/instructionSet.ts
@@ -7,50 +7,47 @@ const instructionSets = [
     "AVRxt"
 ] as const;
 
-type InstructionSet = typeof instructionSets[number];
+type InstructionSet = (typeof instructionSets)[number];
 
 const short = (instructionSet: InstructionSet): string =>
     instructionSet.slice(-1).toLowerCase();
 
-const missing = {
-    "ADIW": "c",
-    "BREAK": "r",
-    "DES": "rc",
-    "EICALL": "rec",
-    "EIJMP": "rec",
-    "ELPM": "rec",
-    "ELPM.Z": "rec",
-    "ELPM.Z+": "rec",
-    "FMUL": "rec",
-    "FMULS": "rec",
-    "FMULSU": "rec",
-    "JMP": "rc",
-    "LAC": "re+tc",
-    "LAS": "re+tc",
-    "LAT": "re+tc",
-    "LDD.Y": "c",
-    "LDD.Z": "c",
-    "LPM": "c",
-    "LPM.Z": "rc",
-    "LPM.Z+": "rc",
-    "MOVW": "rc",
-    "MUL": "rec",
-    "MULS": "rec",
-    "MULSU": "rec",
-    "SBIW": "c",
-    "SPM": "rc",
+const missing: Map<string, string> = new Map([
+    ["ADIW", "c"],
+    ["BREAK", "r"],
+    ["DES", "rc"],
+    ["EICALL", "rec"],
+    ["EIJMP", "rec"],
+    ["ELPM", "rec"],
+    ["ELPM.Z", "rec"],
+    ["ELPM.Z+", "rec"],
+    ["FMUL", "rec"],
+    ["FMULS", "rec"],
+    ["FMULSU", "rec"],
+    ["JMP", "rc"],
+    ["LAC", "e+tc"],
+    ["LAS", "e+tc"],
+    ["LAT", "e+tc"],
+    ["LDD.Y", "c"],
+    ["LDD.Z", "c"],
+    ["LPM", "c"],
+    ["LPM.Z", "rc"],
+    ["LPM.Z+", "rc"],
+    ["MOVW", "rc"],
+    ["MUL", "rec"],
+    ["MULS", "rec"],
+    ["MULSU", "rec"],
+    ["SBIW", "c"],
+    ["SPM", "rc"],
     // TODO: there was a duplicate key here.
     // we need to work out what to do about it!
-    "SPM.Z+": "re+c"
+    ["SPM.Z+", "re+c"]
     // "x" isn't a valid option????
     //"SPM.Z+": "re+xc"
-};
-
-type MissingOpCode = keyof typeof missing;
+]);
 
 export const isMissing = (
     opCode: string,
     instructionSet: InstructionSet
 ): boolean =>
-    opCode in missing &&
-    missing[opCode as MissingOpCode].includes(short(instructionSet));
+    missing.has(opCode) && missing.get(opCode)!.includes(short(instructionSet));
