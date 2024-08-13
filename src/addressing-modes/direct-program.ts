0bedfc979d0d072981d932e02f2a229c2a25a686
Andy Preston <edgeeffect@gmail.com>
2024-08-13 21:18:22 +0100

    new style direct-program with tests

--- src/addressing-modes/direct-program.ts
+++ src/addressing-modes/direct-program.ts
@@ -1,24 +1,27 @@
 import { type GeneratedCode, template } from "../generate/mod.ts";
-import type { Instruction } from "../instruction/mod.ts";
-import { check, checkCount } from "../operands/mod.ts";
+import {
+    type OperandConverter,
+    type SymbolicOperands,
+    checkCount
+} from "../operands/mod.ts";
+import type { Mnemonic } from "../tokens/tokens.ts";
 
 const mapping: Map<string, string> = new Map([
     ["CALL", "1"],
-    ["JMP", "0"],
+    ["JMP", "0"]
 ]);
 
 export const encode = (
-    instruction: Instruction,
-    _programCounter: number
+    mnemonic: Mnemonic,
+    operands: SymbolicOperands,
+    convert: OperandConverter
 ): GeneratedCode | undefined => {
-    if (!mapping.has(instruction.mnemonic)) {
+    if (!mapping.has(mnemonic)) {
         return undefined;
     }
-    checkCount(instruction.operands, ["address"]);
-    check("address", 0, instruction.operands[0]!);
-    const operationBit = mapping.get(instruction.mnemonic)!;
-    return template(
-        `1001_010k kkkk_11${operationBit}k kkkk_kkkk kkkk_kkkk`,
-        [["k", instruction.operands[0]!]]
-    );
+    checkCount(operands, ["address"]);
+    const operationBit = mapping.get(mnemonic)!;
+    return template(`1001_010k kkkk_11${operationBit}k kkkk_kkkk kkkk_kkkk`, [
+        ["k", convert.numeric("address", operands[0]!)]
+    ]);
 };
