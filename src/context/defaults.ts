ec009edbe599b7af521f3dea3b2b2fa168972bb2
Andy Preston <edgeeffect@gmail.com>
2024-08-24 17:44:33 +0100

    The registers in the context are read-only

--- src/context/defaults.ts
+++ src/context/defaults.ts
@@ -1,22 +1,40 @@
+//import { GeneratedCode } from "../generate/mod.ts";
+
 type ContextFunction = (n: number) => number;
 
-export interface Context {
-    [name: string]: number | ContextFunction;
+type Context = Record<string, number|ContextFunction>;
+
+const registers = (context: Context) => {
+    for (let r = 0; r < 32; r++) {
+        Object.defineProperty(context, `R${r}`, {
+            "configurable": false,
+            "value": r,
+            "writable": false
+        });
+    }
+    const specials: Array<[string, number]> = [
+        ['X', 26],
+        ['XL', 26],
+        ['XH', 27],
+        ['Y', 28],
+        ['YL', 28],
+        ['YH', 29],
+        ['Z', 30],
+        ['ZL', 30],
+        ['ZH', 31]
+    ];
+    for (const [name, index] of specials) {
+        Object.defineProperty(context, name, {
+            "configurable": false,
+            "value": index,
+            "writable": false
+        });
+    }
 }
 
 export const defaults = (): Context => {
-    const context: Context = Object.fromEntries(
-        [...Array(32).keys()].map((register) => [`R${register}`, register])
-    );
-    context.X = 26;
-    context.XL = 26;
-    context.XH = 27;
-    context.Y = 28;
-    context.YL = 28;
-    context.YH = 29;
-    context.Z = 30;
-    context.ZL = 30;
-    context.ZH = 31;
+    const context: Context = {};
+    registers(context);
     context.LOW = (n) => n & 0xff;
     context.HIGH = (n) => (n >> 8) & 0xff;
     context.flashOrg = 0;
