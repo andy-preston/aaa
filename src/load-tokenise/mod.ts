f018b0e7ab08e7001f7b346ca77fed1c050df416
Andy Preston <edgeeffect@gmail.com>
2024-08-25 18:52:21 +0100

    Some little bits so I can do a quick integration test

--- src/load-tokenise/mod.ts
+++ src/load-tokenise/mod.ts
@@ -1,5 +1,3 @@
 export type { Mnemonic, Tokens } from "./types.ts";
-
 export { lineTokens } from "./tokens.ts";
-
 export { newLineLoader } from "./line-loader.ts";
---
+++ src/main-loop/cli.ts
@@ -0,0 +1,3 @@
+import { load } from "./main-loop.ts";
+
+load("file1.txt");
