27dc32c1ddd8e69a65e3268e52498af90f93d513
Andy Preston <edgeeffect@gmail.com>
2024-08-26 22:18:47 +0100

    prototype intel hex format encoder (mk 2)

--- src/main-loop/intel-hex.ts
+++ src/main-loop/intel-hex.ts
@@ -1,23 +1,71 @@
-export const intelHex = (bytes: Array<number>) => {
+import { GeneratedCode } from "../generate/mod.ts";
+// twosComplement shouldn't be in operands if it's finding use out here
+import { twosComplement } from "../operands/twos-complement.ts";
+import { newFile } from "./file.ts";
+
+export const newHex = () => {
+    const recordStash: Array<string> = [];
+
+    const codeStash: Array<number> = [];
+
+    let stashedAddress: number = 0;
+
     const hex = (value: number, digits: number) =>
         value.toString(16).toUpperCase().padStart(digits, '0');
 
-    // extended segment address always zero unless something has more
-    // than 64K of flash - bear in mind this applies to the ATMega 1284
-    // which I want to support
-    const oo = [':020000020000FC'];
-    for (let i = 0; i < bytes.length; i += 16) {
-        const page = bytes.slice(i, i + 16);
-        let o = `:${hex(page.length,2)}${hex(i,4)}00`;
-        let s = page.length + ((i >> 8) & 0xff) + (i & 0xff);
-        for (let j = 0; j < page.length; j++) {
-            o += hex(page[j]!, 2);
-            s += page[j]!;
+    const aRecord = (address: number, bytes: number) => {
+        const byteBuffer: Array<string> = [];
+        let sum = 0;
+        for (let index = 0; index < bytes; index += 2) {
+            const first = codeStash.shift()!;
+            const second = codeStash.shift()!;
+            // Flip 'em over to make 'em big endian!
+            byteBuffer.push(hex(second, 2));
+            byteBuffer.push(hex(first, 2));
+            sum += (first + second);
         }
-        s = ((~s) + 1) & 0xff;
-        o += hex(s, 2);
-        oo.push(o);
+        const checksum = twosComplement(sum & 0x0f, 8, true);
+        return [
+            ":",
+            hex(bytes, 2), // it's usually 8, 16 or 32 some warez don't like 32
+            hex(address, 4), // for more than 64K use extended segment address
+            "00", // Data
+            byteBuffer.join(""),
+            hex(checksum, 2)
+        ].join("");
+    };
+
+    const saveRecords = (limit: number) => {
+        let address = stashedAddress;
+        while (codeStash.length > limit) {
+            const bytes = Math.min(16, codeStash.length);
+            recordStash.push(aRecord(address, bytes));
+            address += bytes / 2;
+        }
+    };
+
+    const add = (address: number, code: GeneratedCode) => {
+        const expectedAddress = stashedAddress + codeStash.length / 2;
+        if (address != expectedAddress) {
+            saveRecords(0);
+            stashedAddress = address;
+        }
+        code.forEach((byte) => codeStash.push(byte));
+        if (codeStash.length >= 16) {
+            saveRecords(16);
+        }
+    };
+
+    const save = (fileName: string) => {
+        const theFile = newFile(fileName, ".hex");
+        // extended segment address always zero unless something has more
+        // than 64K of flash - bear in mind this applies to the ATMega 1284
+        // which I do want to support
+        theFile.writeLine(":020000020000FC");
+        recordStash.forEach(theFile.writeLine);
+        theFile.writeLine(":00000001FF");
+        theFile.close();
     }
-    oo.push(':00000001FF\n');
-    return oo.join('\n');
+
+    return { "add": add, "save": save };
 };
