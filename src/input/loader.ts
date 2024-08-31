c9d729c57682dcb6a04faa8fb60a9989a81d69f4
Andy Preston <edgeeffect@gmail.com>
2024-08-31 12:30:53 +0100

    separate file-stack from loader

--- src/input/loader.ts
+++ src/input/loader.ts
@@ -2,11 +2,9 @@ import type { OurContext } from "../context/mod.ts";
 import type { Generate, GeneratedCode } from "../generate/mod.ts";
 import type { ListingLine } from "../output/mod.ts";
 import type { LineTokens } from "../tokens/mod.ts";
+import { fileStack } from "./file-stack.ts";
 import type { LoadLine } from "./line-loader.ts";
-
-type FileName = string;
-
-type StackEntry = [FileName, IterableIterator<[number, string]>];
+import type { FileName } from "./types.ts";
 
 type Line = [FileName, number, string];
 
@@ -17,33 +15,25 @@ export const createLoader = (
     generate: Generate,
     listingLine: ListingLine
 ) => {
-    const fileStack: Array<StackEntry> = [];
-
-    const include = (fileName: string) => {
-        fileStack.push([
-            fileName,
-            Deno.readTextFileSync(fileName).split("\n").entries()
-        ]);
-    };
+    const files = fileStack();
 
     const lines = function* (): Generator<Line, undefined, undefined> {
         let next: IteratorResult<[number, string]>;
-        let thisFile: StackEntry;
-        while (fileStack.length > 0) {
-            const top = fileStack.length - 1;
-            thisFile = fileStack[top]!;
-            next = thisFile[1].next();
+        let file = files.file();
+        while (file != undefined) {
+            next = file[1].next();
             if (next.done) {
-                fileStack.pop();
+                files.drop();
             } else {
                 const [lineNumber, text] = next.value;
-                yield [thisFile[0], lineNumber, text];
+                yield [file[0], lineNumber, text];
             }
+            file = files.file();
         }
     };
 
     const load = (fileName: string) => {
-        include(fileName);
+        files.add(fileName);
         let errorMessage: string;
         let code: GeneratedCode;
         for (const [fileName, lineNumber, rawLine] of lines()) {
@@ -70,7 +60,7 @@ export const createLoader = (
     };
 
     return {
-        "include": include,
+        "include": files.add,
         "load": load
     };
 };
