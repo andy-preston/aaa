4022558c592658bd4b3c1d5bff6ca97ba2b7f22c
Andy Preston <edgeeffect@gmail.com>
2024-09-01 11:43:46 +0100

    made the lister pretty

--- src/output/lister.ts
+++ src/output/lister.ts
@@ -4,36 +4,49 @@ import type { OutputWriteLine } from "./file.ts";
 
 export const lister = (writeLine: OutputWriteLine) => {
     let currentFileName = "";
+    const addressWidth = 5;
+    const objectWidth = 11;
 
     const listFileName = (name: FileName) => {
         if (name != currentFileName) {
-            writeLine(`\n${name}\n${"=".repeat(name.length)}\n`);
+            const underline = "=".repeat(name.length);
+            writeLine(`\n${name}\n${underline}\n`);
             currentFileName = name;
         }
     };
+
+    const source = (lineNumber: number, sourceLine: string) => {
+        const line = `${lineNumber + 1}`.padStart(4, " ");
+        return `${line} ${sourceLine}`;
+    };
+
+    const object = (address: number, code: GeneratedCode) => {
+        const addressHex = address
+            .toString(16)
+            .toUpperCase()
+            .padStart(addressWidth, "0");
+        const object = code
+            .map((byte) => byte.toString(16).padStart(2, "0"))
+            .join(" ")
+            .toUpperCase()
+            .padEnd(objectWidth, " ");
+        return `${addressHex} ${object}`;
+    };
+
+    const arrow = ">".repeat(addressWidth + 1 + objectWidth);
 
     return (
         sourceFile: FileName,
         lineNumber: number,
         address: number,
-        generatedCode: GeneratedCode,
-        source: string,
+        code: GeneratedCode,
+        sourceLine: string,
         errorMessage: string
     ) => {
         listFileName(sourceFile);
-        const lineNumberString = `${lineNumber + 1}`.padStart(4, " ");
-        const addressString = address
-            .toString(16)
-            .padStart(5, "0")
-            .toUpperCase();
-        const object = generatedCode
-            .map((byte) => byte.toString(16).padStart(2, "0"))
-            .join(" ")
-            .padEnd(11, " ")
-            .toUpperCase();
-        writeLine(`${addressString} ${object} ${lineNumberString} ${source}`);
+        writeLine(`${object(address, code)} ${source(lineNumber, sourceLine)}`);
         if (errorMessage != "") {
-            writeLine(`${">".repeat(17)} ${lineNumberString} ${errorMessage}`);
+            writeLine(`${arrow} ${source(lineNumber, errorMessage)}`);
         }
     };
 };
