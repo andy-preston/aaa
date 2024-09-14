ddd5be050669313861a87be0632f335edeaf89da
Andy Preston <edgeeffect@gmail.com>
2024-09-14 16:13:08 +0100

    File loader is concerned with JUST loading

--- src/input/cli.ts
+++ src/input/cli.ts
@@ -1,25 +1,41 @@
 import { createOurContext } from "../context/mod.ts";
 import { directives } from "../directives/list.ts";
-import { generator } from "../generate/mod.ts";
+import { GeneratedCode, generator } from "../generate/mod.ts";
 import { outputter } from "../output/mod.ts";
-import { lineTokens } from "../tokens/mod.ts";
 import { fileLoader } from "./file-loader.ts";
-import { lineLoader } from "./line-loader.ts";
+import { sourceLine } from "../source-line/mod.ts";
 
 const topFile = "./file1.txt";
 
 const ourContext = createOurContext();
 const output = outputter(topFile);
-const loader = fileLoader(
-    ourContext,
-    lineLoader(ourContext),
-    lineTokens,
-    generator(ourContext),
-    output.output
-);
+const tokensFromLine = sourceLine(ourContext);
+const generate = generator(ourContext);
+const loader = fileLoader(topFile);
+
 ourContext.addDirective(["include", loader.include]);
 for (const directive of directives) {
     ourContext.addDirective(directive);
 }
-loader.load(topFile);
+
+let errorMessage: string;
+let code: GeneratedCode;
+for (const [fileName, lineNumber, rawLine] of loader.lines()) {
+    const flashAddress = ourContext.flashPos();
+    try {
+        errorMessage = "";
+        code = generate(tokensFromLine(rawLine));
+    } catch (error) {
+        errorMessage = `${error.name}: ${error.message}`;
+        code = [];
+    }
+    output.output(
+        fileName,
+        lineNumber,
+        flashAddress,
+        code,
+        rawLine,
+        errorMessage
+    );
+}
 output.close();
--- src/input/file-loader.ts
+++ src/input/file-loader.ts
@@ -1,21 +1,11 @@
-import type { OurContext } from "../context/mod.ts";
-import type { Generate, GeneratedCode } from "../generate/mod.ts";
-import type { Output } from "../output/mod.ts";
-import type { LineTokens } from "../tokens/mod.ts";
 import { fileStack } from "./file-stack.ts";
-import type { LoadLine } from "./line-loader.ts";
 import type { FileName } from "./types.ts";
 
 type Line = [FileName, number, string];
 
-export const fileLoader = (
-    ourContext: OurContext,
-    loadLine: LoadLine,
-    lineTokens: LineTokens,
-    generate: Generate,
-    output: Output
-) => {
+export const fileLoader = (topFileName: FileName) => {
     const files = fileStack();
+    files.add(topFileName);
 
     const lines = function* (): Generator<Line, undefined, undefined> {
         let next: IteratorResult<[number, string]>;
@@ -25,39 +15,15 @@ export const fileLoader = (
             if (next.done) {
                 files.drop();
             } else {
-                const [lineNumber, text] = next.value;
-                yield [file[0], lineNumber, text];
+                const [lineNumber, rawLine] = next.value;
+                yield [file[0], lineNumber, rawLine];
             }
             file = files.file();
         }
     };
 
-    const load = (fileName: string) => {
-        files.add(fileName);
-        let errorMessage: string;
-        let code: GeneratedCode;
-        for (const [fileName, lineNumber, rawLine] of lines()) {
-            const flashAddress = ourContext.flashPos();
-            try {
-                errorMessage = "";
-                code = generate(lineTokens(loadLine(rawLine)));
-            } catch (error) {
-                errorMessage = `${error.name}: ${error.message}`;
-                code = [];
-            }
-            output(
-                fileName,
-                lineNumber,
-                flashAddress,
-                code,
-                rawLine,
-                errorMessage
-            );
-        }
-    };
-
     return {
         "include": files.add,
-        "load": load
+        "lines": lines
     };
 };
