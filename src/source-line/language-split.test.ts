35e66a6f243b5c7e3c5a5a187fa41aabf736a4c7
Andy Preston <edgeeffect@gmail.com>
2024-09-22 15:19:23 +0100

    language split (pt 3)

--- src/source-line/language-split.test.ts
+++ src/source-line/language-split.test.ts
@@ -3,23 +3,23 @@ import { createOurContext } from "../context/mod.ts";
 import { languageSplit } from "./language-split.ts";
 
 Deno.test("JS can be delimited with moustaches on the same line", () => {
-    const loader = languageSplit(createOurContext());
-    const result = loader("MOV {{ this.test = 27; return this.test; }}, R2");
+    const split = languageSplit(createOurContext());
+    const result = split("MOV {{ this.test = 27; return this.test; }}, R2");
     assertEquals(result, "MOV 27, R2");
 });
 
 Deno.test("JS can use registers from the context", () => {
-    const loader = languageSplit(createOurContext());
-    const result = loader("MOV {{ R6 }}, R2");
+    const split = languageSplit(createOurContext());
+    const result = split("MOV {{ R6 }}, R2");
     assertEquals(result, "MOV 6, R2");
 });
 
 Deno.test("JS can be delimited by moustaches across several lines", () => {
-    const loader = languageSplit(createOurContext());
-    const firstResult = loader("some ordinary stuff {{ this.test = 27;").trim();
+    const split = languageSplit(createOurContext());
+    const firstResult = split("some ordinary stuff {{ this.test = 27;").trim();
     assertEquals(firstResult, "some ordinary stuff");
-    const secondResult = loader('this.andThat = "hello";');
+    const secondResult = split('this.andThat = "hello";');
     assertEquals(secondResult, "");
-    const thirdResult = loader("return this.andThat; }} matey!");
+    const thirdResult = split("return this.andThat; }} matey!");
     assertEquals(thirdResult, "hello matey!");
 });
