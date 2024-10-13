7a4c7581125b7999c30f94a944427ff031564bfd
Andy Preston <edgeeffect@gmail.com>
2024-10-13 13:27:58 +0100

    Rudimentary static analysis done for now

--- analysis/import-graph.ts
+++ analysis/import-graph.ts
@@ -1,6 +1,6 @@
 import { ImportDeclaration, Project, SourceFile } from "ts_morph";
 
-const cwd = `${Deno.cwd()}/src/`;
+const cwd = `${Deno.cwd()}/src`;
 
 const shortPath = (path: string) => path.replace(cwd, "");
 
@@ -8,7 +8,7 @@ const importsFromDeclaration = function* (
     importingFile: string,
     importingDirectory: string,
     importDeclaration: ImportDeclaration
-) {
+): Generator<[string, string, string], void, unknown> {
     const importedFile = importDeclaration.getModuleSpecifierValue();
     const adjustedFile = importedFile.startsWith("./")
         ? importedFile.replace("./", importingDirectory)
@@ -25,7 +25,11 @@ const importsFromSource = function* (sourceFile: SourceFile) {
         const importingDirectory =
             `${shortPath(sourceFile.getDirectoryPath())}/`;
         for (const importDeclaration of sourceFile.getImportDeclarations()) {
-            yield* importsFromDeclaration(importingFile, importingDirectory, importDeclaration);
+            yield* importsFromDeclaration(
+                importingFile,
+                importingDirectory,
+                importDeclaration
+            );
         }
     }
 };
@@ -38,6 +42,74 @@ const allImports = function* () {
     }
 };
 
-for(const anImport of allImports()) {
-    console.log(anImport);
+type IdentifierMap = Map<string, Array<string>>;
+
+const groups: IdentifierMap = new Map();
+const relationships: IdentifierMap = new Map();
+const labels: Map<string, string> = new Map();
+
+const addToMap = (map: IdentifierMap, file: string, identifier: string) => {
+    if (!map.has(file)) {
+        map.set(file, [identifier]);
+    } else {
+        const identifiers = map.get(file)!;
+        if (!identifiers.includes(identifier)) {
+            identifiers.push(identifier);
+        }
+    }
+};
+
+const addLabel = (qualifiedIdentifier: string, label: string) => {
+    if (!labels.has(qualifiedIdentifier)) {
+        labels.set(qualifiedIdentifier, label);
+    }
 }
+
+const nameForGraph = (fileName: string) =>
+    `_${fileName.replace(/^\//, "").replace(/[\/\.]/g, "_")}`;
+
+const labelFor = (identifier: string) =>
+    labels.has(identifier) ? labels.get(identifier) : identifier;
+
+for (const anImport of allImports()) {
+    const [importingFile, importedFile, identifierLabel] = anImport;
+
+    const identifierGroup = nameForGraph(importedFile);
+    const qualifiedIdentifier =
+        `${identifierGroup}.${nameForGraph(identifierLabel)}`;
+    addLabel(qualifiedIdentifier, identifierLabel);
+    addToMap(groups, identifierGroup, qualifiedIdentifier);
+
+    const sourceOfImport = nameForGraph(importingFile);
+    addLabel(sourceOfImport, importingFile);
+    addToMap(relationships, sourceOfImport, qualifiedIdentifier);
+}
+
+const encoder = new TextEncoder();
+
+const d2File = Deno.openSync("graph.d2", {
+    create: true,
+    write: true,
+    truncate: true
+});
+
+const write = (output: string) => {
+    d2File.writeSync(encoder.encode(`${output}\n`));
+}
+
+write("direction: right");
+for (const [group, identifiers] of groups) {
+    write(`${group}: ${labelFor(group)}`);
+    for (const identifier of identifiers) {
+        write(`${identifier}: ${labelFor(identifier)}`);
+    }
+}
+for (const [source, identifiers] of relationships) {
+    for (const identifier of identifiers) {
+        if (!groups.has(source)) {
+            write(`${source}: ${labelFor(source)}`);
+        }
+        write(`${source} -> ${identifier}`);
+    }
+}
+d2File.close();
