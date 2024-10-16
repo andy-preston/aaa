d62e3bf3ab68284d028686a14472f24f0ffc332e
Andy Preston <edgeeffect@gmail.com>
2024-10-16 18:52:34 +0100

    Simplified abandoning HEX file if any errors during assembly

--- src/output/mod.ts
+++ src/output/mod.ts
@@ -1,29 +1,26 @@
 import type { FileName } from "../coupling/coupling.ts";
 import type { CodeBlock } from "../translate/mod.ts";
 import { file } from "./file.ts";
-import { newHexFile } from "./hex.ts";
+import { type Hex, newHexFile } from "./hex.ts";
 import { openListing } from "./listing.ts";
 
 export const openOutput = (topFileName: FileName) => {
-    let anyErrors = false;
     const listingFile = file(topFileName, ".lst");
     const listing = openListing(listingFile);
-    const hex = newHexFile();
+    let hex: Hex | undefined = newHexFile();
 
     const codeBlock = (block: CodeBlock) => {
         listing.codeBlock(block);
-        for (const message of block.errors) {
+        block.errors.forEach(message => {
             listing.error(message);
-            anyErrors = true;
-        }
-        if (!anyErrors) {
-            hex.codeBlock(block);
-        }
+            hex = undefined;
+        });
+        hex?.codeBlock(block);
     };
 
     const close = () => {
         listingFile.close();
-        if (!anyErrors) {
+        if (hex) {
             const hexFile = file(topFileName, ".hex");
             hex.save(hexFile.write);
             hexFile.close();
