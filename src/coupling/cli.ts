497a32e8dd1aef90ce102328fab763ac2f239cb6
Andy Preston <edgeeffect@gmail.com>
2024-10-16 20:45:53 +0100

    use code-blocks in CLI and make it more compact

--- src/coupling/cli.ts
+++ src/coupling/cli.ts
@@ -1,28 +1,22 @@
-import { process } from "../translate/mod.ts";
+import { codeBlocksFrom } from "../translate/mod.ts";
 import { openOutput } from "../output/mod.ts";
 import { passes, startPass } from "../state/mod.ts";
 import { sourceLines, sourceCheck } from "../source-code/mod.ts";
 import type { FileName } from "./coupling.ts";
 
 export const cli = (commandLineSourceFile: FileName) => {
-    for (const pass of passes) {
+    passes.forEach(pass => {
         startPass(pass);
         const output = pass == 2
             ? openOutput(commandLineSourceFile)
             : undefined;
-        for (const line of sourceLines(commandLineSourceFile)) {
-            if (output) {
-                output.source(line);
-            }
-            for (const codeBlock of process(line)) {
-                if (output) {
-                    output.codeBlock(codeBlock);
-                }
-            }
-        }
-        if (output) {
-            output.close();
-        }
+        sourceLines(commandLineSourceFile).forEach(line => {
+            output?.source(line);
+            codeBlocksFrom(line).forEach(block => {
+                output?.codeBlock(block);
+            });
+        });
+        output?.close();
         sourceCheck();
-    }
+    });
 };
