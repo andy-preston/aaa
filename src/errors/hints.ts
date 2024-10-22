72dadcb2943dc9e94f605900aee4a54ee21df66b
Andy Preston <edgeeffect@gmail.com>
2024-10-22 19:20:39 +0100

    oops... forgot ioOverflowAlternative

--- src/errors/hints.ts
+++ src/errors/hints.ts
@@ -12,3 +12,19 @@ export const unsupportedInstructionAlternative = (
 ): string => instructionAlternatives.has(mnemonic)
     ? `maybe you could try using ${instructionAlternatives.get(mnemonic)}`
     : "";
+
+const ioAlternatives = new Map([
+    ["SBI", "SBR and STS"],
+    ["SBIC", "LDS and SBRC"],
+    ["SBIS", "LDS and SBRS"],
+    ["CBI", "CBR and STS"],
+    ["IN", "LDS"],
+    ["OUT", "STS"]
+]);
+
+export const ioOverflowAlternative = (
+    mnemonic: Mnemonic
+): string => ioAlternatives.has(mnemonic)
+    // TODO: use correct terminology!
+    ? `maybe you could try using ${ioAlternatives.get(mnemonic)} instead of ${mnemonic} to access data space beyond the usual IO area`
+    : "";
